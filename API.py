a980bc834d3928938ccf65c1c6fd81bda2d0b09b
June1811 <qiw.jutamanee@gmail.com>
2025-03-17 01:14:12 +0700

    Update API.py

--- API.py
+++ API.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 CORS(app)
 
 # ✅ เพิ่ม route สำหรับ `/`
-@app.route("/", methods=["GET", "HEAD"])
+@app.route("/", methods=["GET", "HEAD" ,"POST"])
 def home():
     return jsonify({"message": "OCR API is running!"})
 
